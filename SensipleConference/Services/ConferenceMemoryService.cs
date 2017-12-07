fd64a8c492adf631be5272b8acb1b047e428330c
Bhuvanesh S <bhuvaneshs@sensiple.com>
2017-12-07 17:17:39 +0530

    Initial Commit

---
+++ SensipleConference/Services/ConferenceMemoryService.cs
@@ -0,0 +1,47 @@
+﻿using Shared.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SensipleConference.Services
+{
+    public class ConferenceMemoryService : IConferenceService
+    {
+        private readonly List<ConferenceModel> conferences = new List<ConferenceModel>();
+        public ConferenceMemoryService()
+        {
+            conferences.Add(new ConferenceModel { Id = 1, Name = "Jade", Location = "Chennai", AttendeeTotal = 20 });
+            conferences.Add(new ConferenceModel { Id = 2, Name = "Zircon", Location = "Chennai", AttendeeTotal = 30 });
+        }
+
+        public Task Add(ConferenceModel model)
+        {
+            model.Id = conferences.Max(c => c.Id) + 1;
+            conferences.Add(model);
+            return Task.CompletedTask;
+        }
+
+        public Task<IEnumerable<ConferenceModel>> GetAll()
+        {
+            return Task.Run(() => conferences.AsEnumerable());
+        }
+
+        public Task<ConferenceModel> GetById(int id)
+        {
+            return Task.Run(() => conferences.First(c => c.Id == id));
+        }
+
+        public Task<StatisticsModel> GetStatistics()
+        {
+            return Task.Run(() =>
+            {
+                return new StatisticsModel
+                {
+                    NumberOfAttendees = conferences.Sum(c => c.AttendeeTotal),
+                    AverageConferenceAttendees = (int)conferences.Average(c => c.AttendeeTotal)
+                };
+            });
+        }
+    }
+}
---
+++ SensipleConference/Services/ProposalMemoryService.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Shared.Models;
+
+namespace SensipleConference.Services
+{
+    public class ProposalMemoryService : IProposalService
+    {
+
+        private readonly List<ProposalModel> proposals = new List<ProposalModel>();
+
+        public ProposalMemoryService()
+        {
+            proposals.Add(new ProposalModel
+            { Id=1,ConferenceId=1,Speaker="Scott Hanselman",Title="Understanding .NET Core" });
+            proposals.Add(new ProposalModel
+            { Id=2,ConferenceId=2,Speaker="Scott Allen",Title="Entity Framework Core" });
+            proposals.Add(new ProposalModel
+            { Id = 3, ConferenceId = 3, Speaker = "Damien Edwards", Title = "Understanding Kestrel" });
+        }
+
+        public Task Add(ProposalModel model)
+        {
+            model.Id = proposals.Max(p => p.Id) + 1;
+            proposals.Add(model);
+            return Task.CompletedTask;
+        }
+
+        public Task<ProposalModel> Approve(int proposalId)
+        {
+            return Task.Run(() =>
+            {
+                var proposal = proposals.First(p => p.Id == proposalId);
+                proposal.Approved = true;
+                return proposal;
+            });
+        }
+
+        public Task<IEnumerable<ProposalModel>> GetAll(int conferenceId)
+        {
+           return Task.Run(() => proposals.AsEnumerable());
+        }
+    }
+}

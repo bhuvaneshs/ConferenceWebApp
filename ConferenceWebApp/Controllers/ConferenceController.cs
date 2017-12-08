23c5eadb6f56afda408ed46e3b0eeb9ea964c442
Bhuvanesh S <bhuvaneshs@sensiple.com>
2017-12-08 17:46:08 +0530

    Second Commit

---
+++ ConferenceWebApp/Controllers/ConferenceController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ConferenceWebApp.Services;
+using Shared.Models;
+
+namespace ConferenceWebApp.Controllers
+{
+    public class ConferenceController: Controller
+    {
+        private readonly IConferenceService service;
+        public ConferenceController(IConferenceService service)
+        {
+            this.service = service;
+        }
+
+        public IConferenceService Service { get; }
+
+        public async Task<IActionResult> Index()
+        {
+            ViewBag.title = "Conference Overview";
+            return View(await service.GetAll());
+        }
+
+        public IActionResult Add()
+        {
+            ViewBag.title = "Add Conference";
+            return View(new ConferenceModel());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(ConferenceModel model)
+        {
+            if (ModelState.IsValid)
+                await service.Add(model);
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ ConferenceWebApp/Controllers/ProposalController.cs
@@ -0,0 +1,38 @@
+﻿using ConferenceWebApp.Services;
+using Microsoft.AspNetCore.Mvc;
+using Shared.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConferenceWebApp.Controllers
+{
+    public class ProposalController: Controller
+    {
+        private readonly IConferenceService conferenceService;
+        private readonly IProposalService proposalService;
+
+
+        public ProposalController(IConferenceService conferenceService,IProposalService proposalService)
+        {
+            this.conferenceService = conferenceService;
+            this.proposalService = proposalService;
+        }
+
+        public async Task<IActionResult> Index(int conferenceId)
+        {
+            var conference = await conferenceService.GetById(conferenceId);
+            ViewBag.title = $"proposals for conference {conference.Name} {conference.Location}";
+            ViewBag.conferenceId = conferenceId;
+
+            return View(await proposalService.GetAll(conferenceId));
+        }
+        public IActionResult Add(int conferenceId)
+        {
+            ViewBag.title = 
+                Add 
+        }
+    }
+
+}
